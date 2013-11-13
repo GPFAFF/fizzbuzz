912de91c4c82093ff8b0b93f8b6747b6d34c6af3
Greg Pfaff <greg.pfaff@razorfish.com>
2013-11-13 17:17:50 -0600

    Razorish Code 140 Challenge
    JavaScript Problem

---
+++ fizzbuzz.js
@@ -0,0 +1,14 @@
+<script>
+for (var i = 1; i <= 100; i++){
+  var output = i;
+  if(i%3===0 && i%5===0){
+    output="FizzBuzz"
+  }
+  else if(i%3===0){
+    output="Fizz"
+  }
+  else if(if%5===0){
+    output="Buzz"
+  }
+console.log(output);
+</script>
